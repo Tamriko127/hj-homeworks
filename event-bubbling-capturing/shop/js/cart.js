6991fad6389b48de9ce4134f7e81ed4e662fbb4e
Tamriko127 <tamriko-127@rambler.ru>
2018-05-20 23:06:40 +0300

    Корзина

--- event-bubbling-capturing/shop/js/cart.js
+++ event-bubbling-capturing/shop/js/cart.js
@@ -1,1 +1,9 @@
 'use strict';
+list.addEventListener('click', checkCart);
+
+function checkCart(event) {
+	if ((event.target.tagName === 'A') && (event.target.classList.contains('add-to-cart'))) {
+		event.preventDefault();
+		addToCart(event.target.dataset);
+	};
+}
